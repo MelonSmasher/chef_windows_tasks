1791d9443bf858d812f3a442eef9ab19741e693d
Alex Markessinis <markea@sage.edu>
2017-05-19 11:25:29 -0400

    fixed windows depends

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'markea125@gmail.com'
 license 'MIT'
 description 'Manage Windows scheduled tasks through attributes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 issues_url 'https://github.com/MelonSmasher/chef_windows_tasks/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_windows_tasks' if respond_to?(:source_url)
 supports 'windows'
-depends 'windows', '~> 2.1.1'
+depends 'windows', '>= 2.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
